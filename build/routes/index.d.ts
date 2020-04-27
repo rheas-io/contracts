fe9186896d9cbf4285aa235aeddeee533ee3eaaf
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-04-27 13:07:18 +0530

    added route validator to index

--- build/routes/index.d.ts
+++ build/routes/index.d.ts
@@ -1,4 +1,5 @@
 import { IRoute } from "./route";
 import { IRouter } from "./router";
+import { IRouteValidator } from "./validator";
 import { IRouteRegistrar } from "./routeRegistrar";
-export { IRoute, IRouter, IRouteRegistrar };
+export { IRoute, IRouter, IRouteRegistrar, IRouteValidator };
