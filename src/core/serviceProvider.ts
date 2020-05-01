d9b533f5b63f9c7cd1b112f3a82b2b340e6ec785
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-01 17:56:36 +0530

    service provider contract init

---
+++ src/core/serviceProvider.ts
@@ -0,0 +1,17 @@
+export interface IServiceProvider {
+
+    /**
+     * Registers the service provider to the container. Avoid adding
+     * code that require other services inside this function, as they
+     * may not be registered yet. Use boot function for that purpose.
+     */
+    register(): void;
+
+    /**
+     * Boots the service provider. Inside the boot function
+     * all the app service providers will be registered. So, add
+     * the functions that require other service provider inside this
+     * function.
+     */
+    boot(): void;
+}
