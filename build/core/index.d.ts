b6c5907a16def7beea0ff8959a48a2fb156bf931
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-01 19:22:22 +0530

    ClassOf moved to main index

--- build/core/index.d.ts
+++ build/core/index.d.ts
@@ -1,7 +1,6 @@
-import { ClassOf } from "./class";
 import { IConfig } from "./config";
 import { IServer } from "./server";
 import { IContainer } from "./container";
 import { InstanceHandler } from "./instanceHandler";
 import { IServiceProvider } from "./serviceProvider";
-export { ClassOf, IConfig, IServer, IContainer, InstanceHandler, IServiceProvider };
+export { IConfig, IServer, IContainer, InstanceHandler, IServiceProvider };
--- build/index.d.ts
+++ build/index.d.ts
@@ -1,4 +1,5 @@
+import { ClassOf } from "./core/class";
 import { IRequest } from './core/request';
 import { IDbConnector } from './database/connector';
 import { KeyValue, AnyObject, StringObject } from './core/keyValue';
-export { IDbConnector, KeyValue, AnyObject, StringObject, IRequest };
+export { ClassOf, IDbConnector, KeyValue, AnyObject, StringObject, IRequest };
