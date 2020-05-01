665005c539f8d7ddba10129f322a86127e41efab
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-01 18:01:39 +0530

    added service register properties

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,12 +1,22 @@
-import { IConfig } from "./config";
+import { IServer } from "./server";
+import { IContainer } from "./container";
+import { IServiceProvider } from "./serviceProvider";
 
-export interface IApp {
+export interface IApp extends IContainer, IServer {
 
     startApp(): void;
 
+    boot(): void;
+
+    registerService(name: string, service: IServiceProvider): void;
+
+    bootService(service: IServiceProvider): void;
+
+    isDeferredService(name: string): boolean;
+
     config(key: string): any;
 
-    getConfigurations(): IConfig;
+    getConfigPath(): string;
 
     getRootPath(): string;
 
