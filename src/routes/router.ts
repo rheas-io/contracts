a3bdcd45dbfe6a5dbfa73f61e98bf185cf6ece82
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-01 18:01:21 +0530

    router properties to handle requests

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,17 @@
+import { IRoute } from "./route";
+import { IRequest } from "../core/request";
+import { IResponse } from "../core/response";
 import { IRouteRegistrar } from "./routeRegistrar";
 
 export interface IRouter extends IRouteRegistrar {
 
     cacheRoutes(): void;
+
+    processRequest(request: IRequest, response: IResponse): void;
+
+    matchingRoute(request: IRequest, response: IResponse): IRoute;
+
+    dispatchToRoute(route: IRoute, request: IRequest, response: IResponse): IResponse;
 
     addRegistrar(name: string, registrar: IRouteRegistrar): void;
 
