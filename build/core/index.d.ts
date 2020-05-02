e86428fbc5f0bcbd519cdfd7d756ce1b07fa7bec
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-02 11:07:02 +0530

    added configmanager to index

--- build/core/index.d.ts
+++ build/core/index.d.ts
@@ -1,6 +1,7 @@
 import { IConfig } from "./config";
 import { IServer } from "./server";
 import { IContainer } from "./container";
+import { IConfigManager } from "./configManager";
 import { InstanceHandler } from "./instanceHandler";
 import { IServiceProvider } from "./serviceProvider";
-export { IConfig, IServer, IContainer, InstanceHandler, IServiceProvider };
+export { IConfig, IServer, IContainer, InstanceHandler, IServiceProvider, IConfigManager };
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,8 @@
 import { IConfig } from "./config";
 import { IServer } from "./server";
 import { IContainer } from "./container";
+import { IConfigManager } from "./configManager";
 import { InstanceHandler } from "./instanceHandler";
 import { IServiceProvider } from "./serviceProvider";
 
-export { IConfig, IServer, IContainer, InstanceHandler, IServiceProvider }
+export { IConfig, IServer, IContainer, InstanceHandler, IServiceProvider, IConfigManager }
