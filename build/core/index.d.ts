cc71084b1dbe6a5ba078daf75c2acaa74e26965d
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-04 17:45:48 +0530

    server creator added to index

--- build/core/index.d.ts
+++ build/core/index.d.ts
@@ -1,7 +1,8 @@
 import { IConfig } from "./config";
 import { IServer } from "./server";
 import { IContainer } from "./container";
+import { IServerCreator } from "./serverCreator";
 import { IConfigManager } from "./configManager";
 import { InstanceHandler } from "./instanceHandler";
 import { IServiceProvider } from "./serviceProvider";
-export { IConfig, IServer, IContainer, InstanceHandler, IServiceProvider, IConfigManager };
+export { IConfig, IServer, IServerCreator, IContainer, InstanceHandler, IServiceProvider, IConfigManager };
