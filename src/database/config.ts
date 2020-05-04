113b854f36847f2359a7ce60791b11adc7277009
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-04 23:09:45 +0530

    config don't need connector details

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,9 +1,6 @@
-import { IDbConnector } from "./connector";
-
 export interface IDbConfig {
     host: string,
     port: number,
     database: string,
     options: {},
-    connector: IDbConnector
 }
