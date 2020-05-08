a54d3f3cc745ab1c75c502d64760a41498f5cb46
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-08 18:19:51 +0530

    removed service handlers

--- build/core/app.d.ts
+++ build/core/app.d.ts
@@ -1,13 +1,7 @@
 import { IServer } from "./server";
 import { IContainer } from "./container";
-import { IServiceProvider } from "./serviceProvider";
-export interface IApp extends IContainer, IServer, IServiceProvider {
+export interface IApp extends IContainer, IServer {
     startApp(): void;
-    registerService(name: string, serviceProvider: IServiceProvider): void;
-    registerServiceByName(key: string): void;
-    isServiceLoaded(name: string): boolean;
-    bootService(service: IServiceProvider): void;
-    isDeferredService(name: string): boolean;
     config<T>(key: string, defaultValue?: T): T | null;
     getRootPath(): string;
     getAssetPath(): string;
