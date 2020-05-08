076087db87a2619e56247ae23ddf414039b8f3b4
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-08 18:43:21 +0530

    service manager loads deferred services

---
+++ src/core/serviceManager.ts
@@ -0,0 +1,11 @@
+import { IServiceProvider } from "./serviceProvider";
+
+export interface IServiceManager extends IServiceProvider {
+    /**
+     * Loads a deferred service if it's not alreayd registered.
+     * 
+     * @param key 
+     * @return true if service loaded
+     */
+    loadDeferredService(key: string): boolean;
+}
