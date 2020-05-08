7fdeb45e8c33256d76c46a1f086c290abb90b372
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-09 00:33:55 +0530

    service contracts moved to dedicated dir

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+import { IServiceManager } from "./serviceManager";
+import { IServiceProvider } from "./serviceProvider";
+import { IDeferredService } from "./deferredService";
+
+export { IServiceManager, IServiceProvider, IDeferredService }
