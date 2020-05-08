e7959c3790278a11807c1c8b599bcbb0a88e553d
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-08 18:45:59 +0530

    servicemanager added to index

--- build/core/index.d.ts
+++ build/core/index.d.ts
@@ -3,6 +3,7 @@ import { IServer } from "./server";
 import { IManager } from "./manager";
 import { IContainer } from "./container";
 import { IServerCreator } from "./serverCreator";
+import { IServiceManager } from "./serviceManager";
 import { InstanceHandler } from "./instanceHandler";
 import { IServiceProvider } from "./serviceProvider";
-export { IConfig, IServer, IServerCreator, IContainer, InstanceHandler, IServiceProvider, IManager };
+export { IConfig, IServer, IManager, IServerCreator, IContainer, InstanceHandler, IServiceProvider, IServiceManager };
--- src/core/index.ts
+++ src/core/index.ts
@@ -3,9 +3,10 @@ import { IServer } from "./server";
 import { IManager } from "./manager";
 import { IContainer } from "./container";
 import { IServerCreator } from "./serverCreator";
+import { IServiceManager } from "./serviceManager";
 import { InstanceHandler } from "./instanceHandler";
 import { IServiceProvider } from "./serviceProvider";
 
 export {
-    IConfig, IServer, IServerCreator, IContainer, InstanceHandler, IServiceProvider, IManager
+    IConfig, IServer, IManager, IServerCreator, IContainer, InstanceHandler, IServiceProvider, IServiceManager
 }
