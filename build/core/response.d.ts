fc8146dc560435ad79cff45be1f71f30e72111b6
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-13 23:46:46 +0530

    redirect property added

--- build/core/response.d.ts
+++ build/core/response.d.ts
@@ -1,4 +1,11 @@
 /// <reference types="node" />
 import { ServerResponse } from "http";
 export interface IResponse extends ServerResponse {
+    /**
+     * Creates a redirect response.
+     *
+     * @param to
+     * @param status
+     */
+    redirect(to: string, status: number): IResponse;
 }
--- src/core/response.ts
+++ src/core/response.ts
@@ -2,4 +2,11 @@ import { ServerResponse } from "http";
 
 export interface IResponse extends ServerResponse {
 
+    /**
+     * Creates a redirect response.
+     * 
+     * @param to 
+     * @param status 
+     */
+    redirect(to: string, status: number): IResponse;
 }
