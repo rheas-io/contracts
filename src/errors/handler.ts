685c99441241c4b24b2b5e58062a3ed63b06a05d
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-14 17:12:23 +0530

    exception binder to response added

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -1,3 +1,16 @@
-export interface IExceptionHandler {
+import { IException } from "./exception";
+import { IRequest } from "../core/request";
+import { IResponse } from "../core/response";
 
+export interface IExceptionHandler {
+    /**
+     * Binds the exception to the response. Status code, message, body
+     * of the response are updated in according to the exception and returns
+     * the response.
+     * 
+     * @param err 
+     * @param req 
+     * @param res 
+     */
+    responseFromError(err: IException, req: IRequest, res: IResponse): IResponse;
 }
