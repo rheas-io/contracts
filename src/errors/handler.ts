a842ee2fd47a417b0463d6e27c4c31313a91bfed
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-14 20:20:21 +0530

    added exception parser

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -3,6 +3,15 @@ import { IRequest } from "../core/request";
 import { IResponse } from "../core/response";
 
 export interface IExceptionHandler {
+
+    /**
+     * Prepares a rheas exception from the error if it is not 
+     * already one.
+     * 
+     * @param err 
+     */
+    prepareException(err: Error | IException): IException;
+
     /**
      * Binds the exception to the response. Status code, message, body
      * of the response are updated in according to the exception and returns
