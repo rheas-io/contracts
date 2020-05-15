2aa0330253d1887d34f24690ce31c7ef24456dfe
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-15 18:29:38 +0530

    routepipe & handler added to index.

--- build/routes/index.d.ts
+++ build/routes/index.d.ts
@@ -1,5 +1,7 @@
 import { IRoute } from "./route";
 import { IRouter } from "./router";
+import { IRoutePipe } from "./routePipe";
 import { IRouteValidator } from "./validator";
 import { IRouteRegistrar } from "./routeRegistrar";
-export { IRoute, IRouter, IRouteRegistrar, IRouteValidator };
+import { IRoutePipeHandler } from "./routePipeHandler";
+export { IRoute, IRouter, IRoutePipe, IRouteRegistrar, IRouteValidator, IRoutePipeHandler };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import { IRoute } from "./route";
 import { IRouter } from "./router";
+import { IRoutePipe } from "./routePipe";
 import { IRouteValidator } from "./validator";
 import { IRouteRegistrar } from "./routeRegistrar";
+import { IRoutePipeHandler } from "./routePipeHandler";
 
 export {
     IRoute,
     IRouter,
+    IRoutePipe,
     IRouteRegistrar,
-    IRouteValidator
+    IRouteValidator,
+    IRoutePipeHandler
 }
