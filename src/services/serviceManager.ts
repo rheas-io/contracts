2798798d66c7c3557385294c046f0dd02ff1cc03
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-17 20:20:22 +0530

    allows setting custom list of providers.

--- src/services/serviceManager.ts
+++ src/services/serviceManager.ts
@@ -1,6 +1,16 @@
+import { ClassOf } from "../core/class";
+import { KeyValue } from "../core/keyValue";
 import { IServiceProvider } from "./serviceProvider";
 
 export interface IServiceManager extends IServiceProvider {
+
+    /**
+     * Sets the service providers this manager has to handle.
+     * 
+     * @param providers 
+     */
+    setProviders(providers: KeyValue<ClassOf<IServiceProvider>>): void;
+
     /**
      * Loads a deferred service if it's not alreayd registered.
      * 
