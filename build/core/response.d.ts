e117b4e4bf22b1f101094c567c3616573ab0a988
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-19 23:56:07 +0530

    core functions added

--- build/core/response.d.ts
+++ build/core/response.d.ts
@@ -1,6 +1,25 @@
 /// <reference types="node" />
+import { IRequest } from "./request";
 import { ServerResponse } from "http";
 export interface IResponse extends ServerResponse {
+    /**
+     * Sends the response and ends the stream. This completes
+     * a request cycle.
+     */
+    send(): void;
+    /**
+     * Sets the response content/body that has to be send.
+     *
+     * @param content
+     */
+    setContent(content: any): IResponse;
+    /**
+     * Prepares the response with necessary headers and status
+     * code.
+     *
+     * @param request
+     */
+    prepareResponse(request: IRequest): IResponse;
     /**
      * Creates a redirect response.
      *
