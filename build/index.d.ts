2b412c2e555ff6f0111e77bb37c1146a59a71352
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-23 20:31:59 +0530

    response added to index

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,5 +1,6 @@
 import { ClassOf } from "./core/class";
 import { IRequest } from './core/request';
+import { IResponse } from './core/response';
 import { IDbConnector } from './database/connector';
 import { KeyValue, AnyObject, StringObject } from './core/keyValue';
-export { ClassOf, IDbConnector, KeyValue, AnyObject, StringObject, IRequest };
+export { ClassOf, IDbConnector, KeyValue, AnyObject, StringObject, IRequest, IResponse };
