d6e34f8a27353e53a2dd0b68a5708d3f94757b75
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-25 23:43:19 +0530

    promise returns compare and hash maker

--- build/security/hasher.d.ts
+++ build/security/hasher.d.ts
@@ -1,13 +1,21 @@
 export interface IHasher {
     /**
+     * Creates a pepper string for the hash algo.
+     *
+     * @returns string
+     */
+    createPepper(): string;
+    /**
+     * Creates a hash of the value
      *
      * @param value
      */
-    createHash(value: string): string;
+    createHash(value: string): Promise<string>;
     /**
+     * Compares a normal text with hashed value for a match
      *
      * @param value
      * @param hashedValue
      */
-    compare(value: string, hashedValue: string): boolean;
+    compare(value: string, hashedValue: string): Promise<boolean>;
 }
