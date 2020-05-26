858a7b609c56351b2e18f3377aba33749fdee955
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-26 19:54:31 +0530

    pepper creation moved to hashmanager

--- build/security/hashManager.d.ts
+++ build/security/hashManager.d.ts
@@ -1,3 +1,15 @@
 import { IHasher } from "./hasher";
 export interface IHashManager extends IHasher {
+    /**
+     * Creates a pepper string for the hash algo.
+     *
+     * @returns string
+     */
+    createPepper(): Promise<string>;
+    /**
+     * Returns the hash driver to use.
+     *
+     * @returns IHasher
+     */
+    getHasher(): IHasher;
 }
--- src/security/hashManager.ts
+++ src/security/hashManager.ts
@@ -2,4 +2,17 @@ import { IHasher } from "./hasher";
 
 export interface IHashManager extends IHasher {
 
+    /**
+     * Creates a pepper string for the hash algo.
+     * 
+     * @returns string
+     */
+    createPepper(): Promise<string>;
+
+    /**
+     * Returns the hash driver to use.
+     * 
+     * @returns IHasher
+     */
+    getHasher(): IHasher;
 }
