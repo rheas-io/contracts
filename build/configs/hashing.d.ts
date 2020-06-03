36d8d85c24ef9554cb239d9f443267b1bb3d7f81
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-06-03 16:57:16 +0530

    init hashing config contratcs

---
+++ build/configs/hashing.d.ts
@@ -0,0 +1,15 @@
+interface IBcryptConfig {
+    rounds: number;
+}
+interface IArgonConfig {
+    memory: number;
+    threads: number;
+    time: number;
+}
+export interface IHashConfig {
+    key: string;
+    driver: string;
+    bcrypt: IBcryptConfig;
+    argon: IArgonConfig;
+}
+export {};
