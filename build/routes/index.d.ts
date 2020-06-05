44410892994740e0d88f305abb3019ead3fce31f
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-06-05 12:57:52 +0530

    added urlGenerator contract to index

--- build/routes/index.d.ts
+++ build/routes/index.d.ts
@@ -1,7 +1,8 @@
 import { IRoute } from "./route";
 import { IMiddleware } from "./middleware";
 import { IRouteValidator } from "./validator";
+import { IUrlGenerator } from "./urlGenerator";
 import { IRouteRegistrar } from "./routeRegistrar";
 import { IRequestHandler } from "./requestHandler";
 import { IRouter, INameParams, IPipeResolver } from "./router";
-export { IRoute, IRouter, INameParams, IMiddleware, IPipeResolver, IRequestHandler, IRouteRegistrar, IRouteValidator };
+export { IRoute, IRouter, INameParams, IMiddleware, IUrlGenerator, IPipeResolver, IRequestHandler, IRouteRegistrar, IRouteValidator };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { IRoute } from "./route";
 import { IMiddleware } from "./middleware";
 import { IRouteValidator } from "./validator";
+import { IUrlGenerator } from "./urlGenerator";
 import { IRouteRegistrar } from "./routeRegistrar";
 import { IRequestHandler } from "./requestHandler";
 import { IRouter, INameParams, IPipeResolver } from "./router";
@@ -10,6 +11,7 @@ export {
     IRouter,
     INameParams,
     IMiddleware,
+    IUrlGenerator,
     IPipeResolver,
     IRequestHandler,
     IRouteRegistrar,
