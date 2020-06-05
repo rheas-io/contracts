33e19231b66a0ecd99a130a5f2a54433c2c215f8
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-06-05 23:18:48 +0530

    named route getter added

--- build/routes/router.d.ts
+++ build/routes/router.d.ts
@@ -1,16 +1,17 @@
 import { IRoute } from "./route";
+import { IMiddleware } from "./middleware";
 import { IRequest } from "../core/request";
 import { IResponse } from "../core/response";
 import { IRouteRegistrar } from "./routeRegistrar";
-import { IMiddleware } from "./middleware";
 export declare type INameParams = [string, string[]];
 export interface IPipeResolver {
     (nameParams: INameParams): IMiddleware;
 }
 export interface IRouter extends IRouteRegistrar {
-    cacheRoutes(): void;
     handle(request: IRequest, response: IResponse): Promise<IResponse>;
     matchingRoute(request: IRequest): IRoute;
+    getNamedRoute(name: string): IRoute | null;
+    cacheRoutes(): void;
     addRegistrar(name: string, registrar: IRouteRegistrar): void;
     deleteRegistrar(name: string): void;
 }
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,8 @@
 import { IRoute } from "./route";
+import { IMiddleware } from "./middleware";
 import { IRequest } from "../core/request";
 import { IResponse } from "../core/response";
 import { IRouteRegistrar } from "./routeRegistrar";
-import { IMiddleware } from "./middleware";
 
 export type INameParams = [string, string[]];
 
@@ -12,11 +12,13 @@ export interface IPipeResolver {
 
 export interface IRouter extends IRouteRegistrar {
 
-    cacheRoutes(): void;
-
     handle(request: IRequest, response: IResponse): Promise<IResponse>;
 
     matchingRoute(request: IRequest): IRoute;
+
+    getNamedRoute(name: string): IRoute | null;
+
+    cacheRoutes(): void;
 
     addRegistrar(name: string, registrar: IRouteRegistrar): void;
 
