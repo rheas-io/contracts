6754670fe44546c99911d71f81a18625e503a546
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-06-29 21:02:04 +0530

    added controller and auth url generator

--- src/auth/oauthProvider.ts
+++ src/auth/oauthProvider.ts
@@ -1,4 +1,41 @@
+import { IRequest } from "../core/request";
+import { IResponse } from "../core/response";
+
 export interface IOauthProvider {
+
+    /**
+     * Authorization request controller. Redirects the requests to the
+     * provider url where users are asked for account access permissions.
+     * 
+     * @param req 
+     * @param res 
+     */
+    onAuthorizeRequested(req: IRequest, res: IResponse): Promise<IResponse>;
+
+    /**
+     * Returns the authorization request url including the query part
+     * containing state, scope, client_id etc
+     * 
+     * @returns string
+     */
+    getAuthorizationRequestUrl(): string;
+
+    /**
+     * Returns the client_id query string to be used. Keys could be 
+     * different for different providers.
+     * 
+     * @returns string
+     */
+    getClientIdKey(): string;
+
+    /**
+     * Returns the redirect link query string. Keys could be different 
+     * for different providers.
+     * 
+     * @returns string
+     */
+    getRedirectKey(): string;
+
     /**
      * Returns the provider client id
      * 
