05807c5ff88fb2eeea1a161ebf608f727ca6fc17
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-10 23:38:40 +0530

    configs and services returns respective managers.

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,11 +1,35 @@
 import { IServer } from "./server";
+import { IManager } from "./manager";
 import { IContainer } from "../container/container";
+import { IServiceManager } from "../services";
 
 export interface IApp extends IContainer, IServer {
 
+    /**
+     * Starts the application. Boots all the registered services,
+     * creates a database connection and listen for requests.
+     */
     startApp(): void;
 
-    config(key: string, defaultValue?: any): any;
+    /**
+     * Returns the application config manager.
+     * 
+     * @returns
+     */
+    configs(): IManager;
 
-    path(folder: string): string;
+    /**
+     * Returns the application service manager.
+     * 
+     * @returns
+     */
+    services(): IServiceManager;
+
+    /**
+     * Returns path to the folder. If no arg is given, root path
+     * is returned.
+     * 
+     * @param folder 
+     */
+    path(folder?: string): string;
 }
