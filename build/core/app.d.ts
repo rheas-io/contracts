35d45c58b0610af49b927f144819c3e53ffedce9
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-20 01:48:38 +0530

    middleware exception setters/getter added

--- build/core/app.d.ts
+++ build/core/app.d.ts
@@ -27,6 +27,15 @@ export interface IApp extends IContainer, IServer {
      */
     services(): IServiceManager;
     /**
+     * Exception keys setter and getter. Throughout the app certain
+     * exceptions will have to be made to services/operations.These
+     * are set/get using this function.
+     *
+     * @param key
+     * @param value
+     */
+    exceptions(key?: string, value?: string[]): string[];
+    /**
      * Returns path to the folder. If no arg is given, root path
      * is returned.
      *
