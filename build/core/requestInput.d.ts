452578bcc02c7854e460018fcd5efb89b6cd6206
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-20 13:46:36 +0530

    req content and input managers init

---
+++ build/core/requestInput.d.ts
@@ -0,0 +1,21 @@
+import { AnyObject } from "./keyValue";
+export interface IRequestInput {
+    /**
+     * Returns the value of request input/data/file
+     *
+     * @param key
+     */
+    input(key?: string): any;
+    /**
+     * Returns all the request inputs as key value object
+     *
+     * @returns object
+     */
+    all(): AnyObject;
+    /**
+     * Adds the parameter elements to the request inputs.
+     *
+     * @param input
+     */
+    merge(input: AnyObject): IRequestInput;
+}
