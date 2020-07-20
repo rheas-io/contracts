3b0ca382639554bf704a3a1c98d2e929c70f47f6
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-20 13:49:45 +0530

    req content and inputs added to index

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,16 +2,20 @@ import { IApp } from "./app";
 import { IManager } from "./manager";
 import { IHeaders } from "./headers";
 import { IRedirector } from "./redirector";
+import { IRequestInput } from "./requestInput";
 import { ICacheManager } from "./cacheManager";
 import { IAttributeManager } from "./attribute";
 import { IServerCreator } from "./serverCreator";
+import { IRequestContent } from "./requestContent";
 
 export {
     IApp,
     IManager,
     IHeaders,
     IRedirector,
+    IRequestInput,
     ICacheManager,
     IServerCreator,
-    IAttributeManager
+    IRequestContent,
+    IAttributeManager,
 }
