365761c11fcb54945ecf9d1d2b21ec5683252ce6
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-26 16:24:58 +0530

    env, config, service are made container bindings

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,6 +1,4 @@
 import { IServer } from "./server";
-import { IManager } from "./manager";
-import { IServiceManager } from "../services";
 import { IContainer } from "../container/container";
 
 export interface IApp extends IContainer, IServer {
@@ -12,30 +10,12 @@ export interface IApp extends IContainer, IServer {
     startApp(): void;
 
     /**
-     * Returns the application environment variables handler.
-     * 
-     * @returns
-     */
-    env(): IManager;
-
-    /**
-     * Returns the application config manager.
-     * 
-     * @returns
-     */
-    configs(): IManager;
-
-    /**
-     * Returns the application service manager.
-     * 
-     * @returns
-     */
-    services(): IServiceManager;
-
-    /**
      * Exception keys setter and getter. Throughout the app certain 
-     * exceptions will have to be made to services/operations.These 
-     * are set/get using this function.
+     * exceptions will have to be made to services/operations. These 
+     * are set/get using this function. 
+     * 
+     * For example, csrf should be exempt from certain routes. These 
+     * exception route list has to be set on the app instance.
      * 
      * @param key 
      * @param value 
