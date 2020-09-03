d1761189ef65c7efb4a5f385f295ca382dd5f0e2
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-03 23:53:49 +0530

    allowed setting of components outside constructor.

--- src/core/requestParams.ts
+++ src/core/requestParams.ts
@@ -1,3 +1,11 @@
+import { IRequestComponent } from '../routes/uri';
+
 export interface IRequestParams extends Map<string, string> {
-    
+    /**
+     * Sets the parameters from the given uri components. Older
+     * values are cleared before setting new values.
+     *
+     * @param components
+     */
+    setParameters(components: IRequestComponent[]): void;
 }
