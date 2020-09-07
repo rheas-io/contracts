75bb819340dd27a2e424601472a02686db712702
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-07 15:04:51 +0530

    blocking existance checks. reads returns buffer

--- src/files/fileManager.ts
+++ src/files/fileManager.ts
@@ -7,7 +7,7 @@ export interface IFileManager {
      * @param filePath
      * @param module
      */
-    readJsFile(filePath: string, module?: string): any;
+    readJs(filePath: string, module?: string): Promise<null | any>;
 
     /**
      * Read contents of a file and returns as string. Throws an exception if
@@ -16,7 +16,15 @@ export interface IFileManager {
      * @param filePath
      * @param encoding
      */
-    readFile(filePath: string, encoding?: string): Promise<string>;
+    readFile(filePath: string): Promise<Buffer>;
+
+    /**
+     * Reads a file and converts the Buffer to string and returns it.
+     *
+     * @param filePath
+     * @param encoding
+     */
+    readTextFile(filePath: string, encoding?: string): Promise<string>;
 
     /**
      * Read contents of a file synchronously
@@ -24,7 +32,7 @@ export interface IFileManager {
      * @param filePath
      * @param encoding
      */
-    readFileSync(filePath: string, encoding?: string): string;
+    readFileSync(filePath: string): Buffer | null;
 
     /**
      * Returns the file stats of the given path.
@@ -34,6 +42,13 @@ export interface IFileManager {
     fileStats(path: string): Promise<Stats>;
 
     /**
+     * Returns the file stats of the given path. Blocking execution.
+     *
+     * @param path
+     */
+    fileStatsSync(path: string): Stats | null;
+
+    /**
      * Checks if a file exists or not.
      *
      * @param filePath
@@ -41,9 +56,23 @@ export interface IFileManager {
     fileExists(filePath: string): Promise<boolean>;
 
     /**
+     * Blocking check of file existance.
+     *
+     * @param filePath
+     */
+    fileExistsSync(filePath: string): boolean;
+
+    /**
      * Checks if a directory exists or not.
      *
      * @param dirPath
      */
     directoryExists(dirPath: string): Promise<boolean>;
+
+    /**
+     * Blocking checks of directory existance.
+     *
+     * @param dirPath
+     */
+    directoryExistsSync(dirPath: string): boolean;
 }
