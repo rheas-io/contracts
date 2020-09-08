7cfdcf85c9f8ee84fc270e28706899fc111635e5
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-08 23:22:47 +0530

    last accessed replaced with expiry in sessionState

--- src/sessions/sessionState.ts
+++ src/sessions/sessionState.ts
@@ -2,7 +2,7 @@ import { AnyObject } from '../core/keyValue';
 
 export interface ISessionState {
     id: string;
+    expiry: number;
     session: string;
     encrypted: boolean;
-    lastAccessed: number;
 }
