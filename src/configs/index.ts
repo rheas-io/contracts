8f9b54eddcc2a9fa35b34262cfcd85f3cfa74cdf
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-14 20:48:00 +0530

    mail config added for easy access

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,6 +1,7 @@
 import { IAppConfig } from './app';
 import { IAuthConfig } from './auth';
+import { IMailConfig } from './mail';
 import { IDbConfig } from './database';
 import { IHashConfig } from './hashing';
 
-export { IAppConfig, IAuthConfig, IDbConfig, IHashConfig };
+export { IAppConfig, IAuthConfig, IMailConfig, IDbConfig, IHashConfig };
