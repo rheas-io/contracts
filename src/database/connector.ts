7cf078b334d38c686f3110dbd8cf67f42638a9ef
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-16 22:44:33 +0530

    connector caches connection by name

--- src/database/connector.ts
+++ src/database/connector.ts
@@ -10,15 +10,31 @@ export interface IDbConnector<T> {
     connect(): Promise<T>;
 
     /**
-     * Returns an active database connection. If no connection is found, the
-     * function attempts to create one.
+     * Creates a new connection mapped to the given name. If no uri is given,
+     * the uri created from the database config data is used. Returns the new
+     * connection.
      *
      * Throws exception if connection was not made.
+     *
+     * @param name
+     * @param uri
+     *
+     * @throws
+     */
+    createConnection(name: string, uri?: string): Promise<T>;
+
+    /**
+     * Returns a connection mapped to the given name. If no name is given
+     * the default connection is returned.
+     *
+     * Throws exception if a connection for the name is not registered.
+     *
+     * @param name
      *
      * @returns
      * @throws
      */
-    connection(): Promise<T>;
+    connection(name?: string): T;
 
     /**
      * Closes all the database connections. Returns a promise of all connection close
