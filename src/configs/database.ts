299a6b31f06f2b4d143f9d8d37ed3d924655484c
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-18 13:33:09 +0530

    added connector property to db config

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,30 +1,32 @@
 import { IConfig } from './config';
 
 export interface IDbConfig extends IConfig {
+    connector: 'sequelize' | 'mongoose';
+
     /**
      * Application database host.
-     * 
+     *
      * @property
      */
     host: string;
 
     /**
      * Database port number to connect to.
-     * 
+     *
      * @property
      */
     port: number;
 
     /**
      * Name of the collection/database in the db server.
-     * 
+     *
      * @property
      */
     database: string;
 
     /**
      * Additional options for connecting to the database server.
-     * 
+     *
      * @property
      */
     options: {};
