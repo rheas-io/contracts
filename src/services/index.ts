7457542f7df94bbb700c8c3a6ab6a273e9b6591f
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-19 23:53:37 +0530

    removed deferred service as they are lazy loaded

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,5 @@
 import { IDriverManager } from './driverManager';
 import { IServiceManager } from './serviceManager';
 import { IServiceProvider } from './serviceProvider';
-import { IDeferredService } from './deferredService';
 
-export { IDriverManager, IServiceManager, IServiceProvider, IDeferredService };
+export { IDriverManager, IServiceManager, IServiceProvider };
