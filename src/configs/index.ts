7e2fde94351c6f3ea006eaaf06eccd626c4125a9
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-19 16:48:51 +0530

    added sessionConfig to index

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,5 +3,6 @@ import { IAuthConfig } from './auth';
 import { IMailConfig } from './mail';
 import { IDbConfig } from './database';
 import { IHashConfig } from './hashing';
+import { ISessionConfig } from './session';
 
-export { IAppConfig, IAuthConfig, IMailConfig, IDbConfig, IHashConfig };
+export { IAppConfig, IAuthConfig, IMailConfig, IDbConfig, IHashConfig, ISessionConfig };
