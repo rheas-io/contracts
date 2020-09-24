ea6821b7594fcd764fe1f534aaf73cc7e465a5e5
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-25 00:42:59 +0530

    added json type with string/number/null and array

--- src/core/keyValue.ts
+++ src/core/keyValue.ts
@@ -5,3 +5,10 @@ export interface KeyValue<T> {
 export interface AnyObject extends KeyValue<any> {}
 
 export interface StringObject extends KeyValue<string> {}
+
+type JsonTypes = string | number | null;
+type JsonArray = Array<JsonTypes | JsonObject | JsonArray>;
+
+export interface JsonObject {
+    [key: string]: JsonTypes | JsonObject | JsonArray;
+}
