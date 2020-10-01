33fbd9494cc1bfb2f4c8e8669ebb03ec3d004ea8
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-10-01 12:17:58 +0530

    mandatory services contract added

--- src/configs/appProviders.ts
+++ src/configs/appProviders.ts
@@ -3,6 +3,19 @@ import { KeyValue } from '../core/keyValue';
 import { IServiceProvider } from '../services';
 
 export type IAppProviders = KeyValue<ClassOf<IServiceProvider>> & {
+    // Mandatory services that has to be present in the app service
+    // provider configuration file.
+    db: ClassOf<IServiceProvider>;
+    url: ClassOf<IServiceProvider>;
+    cli: ClassOf<IServiceProvider>;
+    mail: ClassOf<IServiceProvider>;
+    view: ClassOf<IServiceProvider>;
+    hash: ClassOf<IServiceProvider>;
+    error: ClassOf<IServiceProvider>;
+    router: ClassOf<IServiceProvider>;
+    kernal: ClassOf<IServiceProvider>;
+    encrypt: ClassOf<IServiceProvider>;
+    middlewares: ClassOf<IServiceProvider>;
     // Add the keys, that should never be added in the
     // config files. These providers are already registered
     // in the container itself.
--- src/configs/requestProviders.ts
+++ src/configs/requestProviders.ts
@@ -3,6 +3,10 @@ import { KeyValue } from '../core/keyValue';
 import { IServiceProvider } from '../services';
 
 export type IRequestProviders = KeyValue<ClassOf<IServiceProvider>> & {
+    // Mandatory services of request lifecycle.
+    cookie: ClassOf<IServiceProvider>;
+    session: ClassOf<IServiceProvider>;
+    redirect: ClassOf<IServiceProvider>;
     // Add the keys, that should never be added in the
     // config files. These providers are already registered
     // in the container itself.
